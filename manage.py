49479bf00a09a84443726e4035168a05537824d3
ebrahim.j <ebrahim.janoowalla@andela.com>
2017-05-23 20:57:44 +0300

    remove unused import

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@ import os
 from flask_script import Manager, prompt_bool # class for handling a set of commands
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app import models
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
