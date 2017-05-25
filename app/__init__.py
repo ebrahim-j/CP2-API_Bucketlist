2b00dbc4894b86d75d9ee1d549f60d59ff993aab
ebrahim.j <ebrahim.janoowalla@andela.com>
2017-05-25 09:57:27 +0300

    Add pagination and search_blist_by_name functionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,7 @@ from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, jsonify, abort, make_response
 
-# local import
 from instance.config import app_config
-
-# initialize sql-alchemy
 db = SQLAlchemy()
 
 
@@ -25,14 +22,12 @@ def create_app(config_name):
         # Get the access token from the header
         auth_header = request.headers.get('Authorization')
         access_token = auth_header.split(" ")[1]
-
         if access_token:
          # Attempt to decode the token and get the User ID
             user_id = User.decode_token(access_token)
             # print (user_id)
             if not isinstance(user_id, str):
                 # Go ahead and handle the request, the user is authenticated
-
                 if request.method == "POST":
                     name = str(request.data.get('name', ''))
                     if name:
@@ -54,24 +49,34 @@ def create_app(config_name):
                         return make_response(response), 400
 
                 else:
-                    # GET all the bucketlists created by this user
-                    bucketlists = Bucketlist.query.filter_by(
-                        created_by=user_id)
-                    if bucketlists:
-                        results = []
+                    # GET bucketlist by name
+                    search = str(request.args.get('q', ''))
+                    if search:
+                        bucketlist = Bucketlist.query.filter_by(
+                            name=search).filter_by(created_by=user_id).first()
 
-                        for bucketlist in bucketlists:
+                        if not bucketlist:
+                            # There is no bucketlist with this ID for this User, so
+                            # Raise an HTTPException with a 404 not found
+                            # status code
+                            obj = {
+                                "message": "No bucketlist found with that name. Remember: Names are spelling+case sensitive"
+                            }
+                            return make_response(jsonify(obj)), 400
+                        else:
                             items = Item.query.filter_by(
                                 bucketlist_id=bucketlist.id)
                             results_items = []
-                            for item in items:
-                                obj = {
-                                    'id': item.item_id,
-                                    'name': item.name,
-                                    'date_created': item.date_created,
-                                    'date_modified': item.date_modified
-                                }
-                                results_items.append(obj)
+                            if items:
+
+                                for item in items:
+                                    obj = {
+                                        'id': item.item_id,
+                                        'name': item.name,
+                                        'date_created': item.date_created,
+                                        'date_modified': item.date_modified
+                                    }
+                                    results_items.append(obj)
                             obj = {
                                 'id': bucketlist.id,
                                 'name': bucketlist.name,
@@ -80,13 +85,93 @@ def create_app(config_name):
                                 'items': results_items,
                                 'created_by': bucketlist.created_by
                             }
-                            results.append(obj)
-                        return make_response(jsonify(results)), 200
+
+                            return make_response(jsonify(obj)), 200
                     else:
-                        response = jsonify({
-                            "message": "No bucketlist yet"
-                        })
-                        return make_response(response), 400
+                        # GET all the bucketlists created by this user
+                        if request.args.get("page"):
+                            page = int(request.args.get("page"))
+                        else:
+                            page = 1
+                        if request.args.get("limit") and int(request.args.get("limit")) < 100:
+                            limit = int(request.args.get("limit"))
+                        else:
+                            limit = 3
+                        bucketlists = Bucketlist.query.filter_by(
+                            created_by=user_id).paginate(page, limit, False)
+                        listed_bucketlists = bucketlists.items
+                        result = []
+                        if bucketlists.has_next:
+                            nextpage = "/api/v1/bucketlists/?page=" + \
+                                str(page + 1) + "&limit=" + str(limit)
+                        else:
+                            nextpage = None
+                        if bucketlists.has_prev:
+                            previouspage = "/api/v1/bucketlists/?page=" + \
+                                str(page - 1) + "&limit=" + str(limit)
+                        else:
+                            previouspage = None
+
+                        if bucketlists:
+                            temp_list = []
+                            for bucketlist in listed_bucketlists:
+                                items = Item.query.filter_by(
+                                    bucketlist_id=bucketlist.id)
+                                results_items = []
+                                for item in items:
+                                    obj = {
+                                        'id': item.item_id,
+                                        'name': item.name,
+                                        'date_created': item.date_created,
+                                        'date_modified': item.date_modified
+                                    }
+                                    results_items.append(obj)
+                                data = {
+                                    'id': bucketlist.id,
+                                    'name': bucketlist.name,
+                                    'date_created': bucketlist.date_created,
+                                    'date_modified': bucketlist.date_modified,
+                                    'items': results_items,
+                                    'created_by': bucketlist.created_by
+                                }
+                                temp_list.append(data)
+                            response = {
+                                "nextpage": nextpage,
+                                "Previouspage": previouspage,
+                                "Buckets": temp_list
+                            }
+                            return make_response(jsonify(response)), 200
+                            # bucketlists = Bucketlist.query.filter_by(
+                            #     created_by=user_id)
+                            # results = []
+
+                            # for bucketlist in bucketlists:
+                            #     items = Item.query.filter_by(
+                            #         bucketlist_id=bucketlist.id)
+                            #     results_items = []
+                            #     for item in items:
+                            #         obj = {
+                            #             'id': item.item_id,
+                            #             'name': item.name,
+                            #             'date_created': item.date_created,
+                            #             'date_modified': item.date_modified
+                            #         }
+                            #         results_items.append(obj)
+                            #     obj = {
+                            #         'id': bucketlist.id,
+                            #         'name': bucketlist.name,
+                            #         'date_created': bucketlist.date_created,
+                            #         'date_modified': bucketlist.date_modified,
+                            #         'items': results_items,
+                            #         'created_by': bucketlist.created_by
+                            #     }
+                            #     results.append(obj)
+                            # return make_response(jsonify(results)), 200
+                        else:
+                            response = jsonify({
+                                "message": "No bucketlist yet"
+                            })
+                            return make_response(response), 400
             else:
                 # user is not legit, so the payload is an error message
                 message = user_id
@@ -98,7 +183,7 @@ def create_app(config_name):
     @app.route('/api/v1/bucketlists/<int:b_id>', methods=['GET', 'PUT', 'DELETE'])
     def bucketlist_manipulation(b_id, **kwargs):
         """ GETS a bucketlist's info, changes it or deletes it by ID"""
-        # retrieve a buckelist using it's ID
+
         # get the access token from the authorization header
         auth_header = request.headers.get('Authorization')
         access_token = auth_header.split(" ")[1]
@@ -110,7 +195,7 @@ def create_app(config_name):
             if not isinstance(user_id, str):
                 # If the id is not a string(error), we have a user id
                 # Get the bucketlist with the id specified from the URL
-                # (<int:id>)
+                # (<int:b_id>)
                 bucketlist = Bucketlist.query.filter_by(
                     created_by=user_id).first()
                 if not bucketlist:
@@ -188,7 +273,7 @@ def create_app(config_name):
             if not isinstance(user_id, str):
                 # If the id is not a string(error), we have a user id
                 # Get the bucketlist with the id specified from the URL
-                # (<int:id>)
+                # (<int:b_id>)
                 bucketlist = Bucketlist.query.filter_by(id=b_id).first()
                 if not bucketlist:
                     # There is no bucketlist with this ID for this User, so
