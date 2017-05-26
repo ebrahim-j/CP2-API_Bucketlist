0fcedeee93de3c79a8514a17cbcd8b90fe2fa070
ebrahim.j <ebrahim.janoowalla@andela.com>
2017-05-26 09:17:55 +0300

    add onupdate modification for items

--- app/models.py
+++ app/models.py
@@ -121,7 +121,8 @@ class Item(db.Model):
     item_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(
+    ), onupdate=db.func.current_timestamp())
     done = db.Column(db.Boolean, default=False)
     bucketlist_id = db.Column(db.Integer, db.ForeignKey(Bucketlist.id))
 
