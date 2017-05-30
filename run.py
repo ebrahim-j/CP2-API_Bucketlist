7760d5559861436770fa974dad0346fc6c55ced9
ebrahim.j <ebrahim.janoowalla@andela.com>
2017-05-30 11:43:14 +0300

    Correct invalid syntax

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app import create_app
 config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
 app = create_app(config_name)
 
-@app.route('/', methods=['GET']):
+@app.route('/', methods=['GET'])
 def index():
     return "Hello world"
 
