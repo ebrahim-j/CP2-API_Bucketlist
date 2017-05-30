ca534b525fea28efa93683e8bc367c99557b8ac1
ebrahim.j <ebrahim.janoowalla@andela.com>
2017-05-30 11:37:27 +0300

    Add index.html to see if path issue is solved

--- run.py
+++ run.py
@@ -5,5 +5,9 @@ from app import create_app
 config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
 app = create_app(config_name)
 
+@app.route('/'):
+def index():
+    return "Hello world"
+
 if __name__ == '__main__':
     app.run()
