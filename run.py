ec6e4d5e5ee2e6303e887b36ca08b4cd20b0ac98
ebrahim.j <ebrahim.janoowalla@andela.com>
2017-05-30 11:40:39 +0300

    Add a get method for index/hompage

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app import create_app
 config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
 app = create_app(config_name)
 
-@app.route('/'):
+@app.route('/', methods=['GET']):
 def index():
     return "Hello world"
 
